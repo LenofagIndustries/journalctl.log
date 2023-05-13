c0eea823d911d7d05c81104ec56e4dc76c986fae
Andrey Stalker <shtieynikov@gmail.com>
2023-05-13 20:37:10 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if ! type node > /dev/null 2>&1 then
+    echo "Node.js is not installed!"
+    exit 2
+fi
+
+if ! type git > /dev/null 2>&1 then
+    echo "git is not installed!"
+    exit 2
+fi
+
+echo "Cloning repository..."
+
+git clone https://github.com/LenofagIndustries/journalctl.log
+if ! -d journalctl.log then
+    echo "Failed to clone repository!"
+    exit 1
+fi
+
+echo "Installing dependencies..."
+cd journalctl.log
+npm install
+
+echo "Done! Configure webhook in config.js."
+echo "pm2 start index.js --name journalctl.log"
