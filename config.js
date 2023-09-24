0088a102b1eea6015a6799db0da59b5a61f10357
Andrey Stalker <shtieynikov@gmail.com>
2023-09-24 04:58:27 +0300

    1.1.0
    Removed discord.js (it is evil)
    Webhook configured by URL now
    Added ntfy alerts for successful SSH logins

--- config.js
+++ config.js
@@ -1,3 +1,10 @@
 module.exports = {
-    webhook: {id: "Webhook ID", token: "Webhook Token"}
+    webhook: {url: "Webhook URL"},
+    ntfy: {
+        enabled: false,
+        hostname: "Your hostname",
+        url: "https://ntfy.sh/sshalerts_etc", // i hope you selfhost ntfy
+        user: "ssh",
+        password: "12345"
+    }
 }
--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 'use strict'
 
 const config = require("./config.js")
-const Discord = require("discord.js")
+const fetch = require("node-fetch")
 const journalctlfuckingconstructor = require("journalctl")
 const journalctl = new journalctlfuckingconstructor();
 
 console.log("journalctl.log: started...")
 
-const webhook = new Discord.WebhookClient({id: config.webhook.id, token: config.webhook.token})
+if(!config.webhook.url) {
+    if(!config.webhook.id || !config.webhook.token) throw "Webhook is not configured! Please put webhook URL to config.js";
+    config.webhook.url = `https://discord.com/api/webhooks/${config.webhook.id}/${config.webhook.token}`
+}
+
+var webhook = {send: (content => {
+    fetch(config.webhook.url, {method: "POST", body: JSON.stringify(typeof content == "string" ? {content} : content), headers: {"Content-Type": "application/json"}})
+        .catch(err => console.log(`journalctl.log: error sending webhook: ${err}`))
+})}
+
 webhook.send({embeds: [{title: "journalctl.log started..."}]})
 
 var queue = [];
-
 function* pastedChunking(e,n){for(let i=0;i<e.length;i+=n)yield e.slice(i,i+n)} // minified https://stackoverflow.com/a/55435856
+
+var sshregex = /^Accepted (publickey|password) for ([A-Za-z0-9-_]+) from ([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}) port ([0-9]{5})/
 
 setInterval(() => {
     if(!queue.length) return;
@@ -20,13 +30,25 @@ setInterval(() => {
     var msg = "";
     if(queue.length >= 10) {var a = [...pastedChunking(queue, 5)]; queue = a[0]; oqueue = oqueue.slice(5); big = true};
     queue.forEach(a => msg += `\n${a}`); queue = big ? oqueue : []; oqueue = []; msg = msg.replaceAll("*", "\\*").replaceAll("`", "\\`").replaceAll("_", "\\_").replaceAll("|", "\\|").replaceAll("~", "\\~").replaceAll("@", "@ ") // pasted myself
-    webhook.send({content: `${msg}`}).catch(err => console.log(`journalctl.log: error sending: ${err}`))
+    webhook.send({content: `${msg}`})
 }, 1000)
 
 journalctl.on("event", data => {
     queue.push(`<${data.PRIORITY}> [${data._HOSTNAME} ${data._SYSTEMD_UNIT ?? data.SYSLOG_IDENTIFIER}]: ${data.MESSAGE}`)
-})
-
-webhook.on("rateLimit", (data) => {
-    console.log(`journalctl.log: webhook rate limited! ends in ${Math.round(data.timeout)}ms`)
+    if(config.ntfy.enabled) {
+        var matched = data.MESSAGE.match(sshregex);
+        if(matched) {
+            var method = matched[1], user = matched[2], ip = matched[3], port = matched[4]
+            if(method && user && ip && port) {
+                fetch(config.ntfy.url, {
+                    method: "POST",
+                    body: `Accepted ${method} for ${user} by ${ip} port ${port}`,
+                    headers: {
+                        "Title": `Successfull SSH login at ${config.ntfy.hostname}`,
+                        "Authorization": `Basic ${btoa(`${config.ntfy.user}:${config.ntfy.password}`)}`
+                    }
+                }).catch(err => console.log(`journalctl.log: error sending to ntfy: ${err}`))
+            }
+        }
+    }
 })
